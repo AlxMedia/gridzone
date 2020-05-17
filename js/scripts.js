433c94f661ec375bfe00fa910c9c345c1da635f1
Alexander Agnarson <alexander.agnarson@gmail.com>
2020-05-17 16:49:13 +0200

    Remove unused script

--- js/scripts.js
+++ js/scripts.js
@@ -167,39 +167,6 @@ jQuery(document).ready(function($) {
 		}
 
 	}, 250);
-	
-/*  Mobile menu smooth toggle height
-/* ------------------------------------ */	
-	$('.nav-toggle').on('click', function() {
-		slide($('.nav-wrap .nav', $(this).parent()));
-	});
-	 
-	function slide(content) {
-		var wrapper = content.parent();
-		var contentHeight = content.outerHeight(true);
-		var wrapperHeight = wrapper.height();
-	 
-		wrapper.toggleClass('expand');
-		if (wrapper.hasClass('expand')) {
-		setTimeout(function() {
-			wrapper.addClass('transition').css('height', contentHeight);
-		}, 10);
-	}
-	else {
-		setTimeout(function() {
-			wrapper.css('height', wrapperHeight);
-			setTimeout(function() {
-			wrapper.addClass('transition').css('height', 0);
-			}, 10);
-		}, 10);
-	}
-	 
-	wrapper.one('transitionEnd webkitTransitionEnd transitionend oTransitionEnd msTransitionEnd', function() {
-		if(wrapper.hasClass('open')) {
-			wrapper.removeClass('transition').css('height', 'auto');
-		}
-	});
-	}
 
 /*  Slick image slide
 /* ------------------------------------ */	
