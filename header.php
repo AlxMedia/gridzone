12511e383569aecc67ffc37f2f057a615b4d5644
Alexander Agnarson <alexander.agnarson@gmail.com>
2020-08-24 13:03:04 +0200

    wrap-nav id and wrap-nav class for all menus

--- header.php
+++ header.php
@@ -28,11 +28,15 @@
 				<?php endif; ?>
 				
 				<?php if ( has_nav_menu('header') ): ?>
-					<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'header','menu_id' => 'nav-header','fallback_cb'=> false)); ?>
+					<div id="wrap-nav-header" class="wrap-nav">
+						<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'header','menu_id' => 'nav-header','fallback_cb'=> false)); ?>
+					</div>
 				<?php endif; ?>
 				
 				<?php if ( has_nav_menu('mobile') ): ?>
-					<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'mobile','menu_id' => 'nav-mobile','fallback_cb'=> false)); ?>
+					<div id="wrap-nav-mobile" class="wrap-nav">
+						<?php \AlxMedia\Nav::nav_menu(array('theme_location'=>'mobile','menu_id' => 'nav-mobile','fallback_cb'=> false)); ?>
+					</div>
 				<?php endif; ?>
 				
 			</div>
