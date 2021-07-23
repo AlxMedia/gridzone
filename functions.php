f302a6a29e7185f092ff85cf7e87262b1ac60027
Alexander Agnarson <alexander.agnarson@gmail.com>
2021-07-23 18:45:33 +0200

    Disable support for widgets block editor, regen lang

--- functions.php
+++ functions.php
@@ -76,6 +76,9 @@ if ( ! function_exists( 'gridzone_setup' ) ) {
 		// Enable support for selective refresh of widgets in customizer
 		add_theme_support( 'customize-selective-refresh-widgets' );
 		
+		// Disable support for widgets block editor
+		remove_theme_support( 'widgets-block-editor' );
+		
 		// Thumbnail sizes
 		add_image_size( 'gridzone-small', 200, 200, true );
 		add_image_size( 'gridzone-medium', 520, 347, true );
